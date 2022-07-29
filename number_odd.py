41e096d5b65ce27a17509d5a967120b7917d7132
Samandar <samandarnfh.2020@gmail.com>
2022-07-29 17:17:30 +0500

    qwer

--- number_odd.py
+++ number_odd.py
@@ -3,3 +3,10 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of odd digits in the variable "var_int".
+var_int=4547
+x1=var_int%10 
+x2=var_int//10%10
+x3=var_int//100%10 
+x4=var_int//1000%10
+s=(x1%2)+(x2%2)+(x3%2)+(x4%2)
+print (s)
