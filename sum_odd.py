24d6803ebfdf786e1c20c9d68ce8a5e1af174ab1
Samandar <samandarnfh.2020@gmail.com>
2022-07-29 17:29:52 +0500

    2-urunish

--- sum_odd.py
+++ sum_odd.py
@@ -5,3 +5,13 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the odd digits in the variable "var_int".
+
+var_int=4561
+sum_juft=0
+x1=var_int%10 
+x2=var_int//10%10
+x3=var_int//100%10 
+x4=var_int//1000%10
+
+#s=(x1%2)+(x2%2)+(x3%2)+(x4%2)
+print(x1)
